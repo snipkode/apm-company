7a86b1f72f183319eeaff597d6b5b6642fd999db
e-compfast <alamhafidz01@gmail.com>
2020-12-29 15:55:23 +0700

    Update Main Branch

---
+++ build/precache-manifest.49848335f4b1620c8615ff2393ba9a5c.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec934f89c14b2fe6ed9a1eb83abc74d4",
+    "url": "https://arkanaputra.com/index.html"
+  },
+  {
+    "revision": "c9c861982a82e3724cc4",
+    "url": "https://arkanaputra.com/static/css/2.724a2e2c.chunk.css"
+  },
+  {
+    "revision": "2ddd700f000a19d7f88f",
+    "url": "https://arkanaputra.com/static/css/main.17a20701.chunk.css"
+  },
+  {
+    "revision": "c9c861982a82e3724cc4",
+    "url": "https://arkanaputra.com/static/js/2.f0a30348.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://arkanaputra.com/static/js/2.f0a30348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddd700f000a19d7f88f",
+    "url": "https://arkanaputra.com/static/js/main.5bae12d0.chunk.js"
+  },
+  {
+    "revision": "d95ef7702e148ffaee85",
+    "url": "https://arkanaputra.com/static/js/runtime-main.e18579cd.js"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/01.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "https://arkanaputra.com/static/media/01.ab7662de.jpg"
+  },
+  {
+    "revision": "c0713f945610c2a385410e94ab1fcc77",
+    "url": "https://arkanaputra.com/static/media/01.c0713f94.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/02.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "https://arkanaputra.com/static/media/02.ab7662de.jpg"
+  },
+  {
+    "revision": "ddab42a4b9f3f2e6f849e60f17539e65",
+    "url": "https://arkanaputra.com/static/media/02.ddab42a4.jpg"
+  },
+  {
+    "revision": "40e47a88525ecbb74b1011dc8d5477a3",
+    "url": "https://arkanaputra.com/static/media/03.40e47a88.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/03.99492839.jpg"
+  },
+  {
+    "revision": "ab7662de93df4d8b97561ea50f853703",
+    "url": "https://arkanaputra.com/static/media/03.ab7662de.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/04.99492839.jpg"
+  },
+  {
+    "revision": "c4a74383482d0305fb710a48a951ed03",
+    "url": "https://arkanaputra.com/static/media/04.c4a74383.jpg"
+  },
+  {
+    "revision": "673f0909f39352fe0531d22db94173d9",
+    "url": "https://arkanaputra.com/static/media/05.673f0909.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/05.99492839.jpg"
+  },
+  {
+    "revision": "97be56efa996bf1edb32dab78e32a575",
+    "url": "https://arkanaputra.com/static/media/06.97be56ef.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/06.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/07.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/08.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/09.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/10.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/11.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/12.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/13.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/14.99492839.jpg"
+  },
+  {
+    "revision": "99492839fdb006e5a18e2865363d7047",
+    "url": "https://arkanaputra.com/static/media/15.99492839.jpg"
+  },
+  {
+    "revision": "829d452c64c4b55779e2140db2c59edb",
+    "url": "https://arkanaputra.com/static/media/404.829d452c.png"
+  },
+  {
+    "revision": "74e067b84271ea8e45ad08de285bdfec",
+    "url": "https://arkanaputra.com/static/media/about-2-bg.74e067b8.jpg"
+  },
+  {
+    "revision": "3e593410f75fa5d081f3a18e6f563359",
+    "url": "https://arkanaputra.com/static/media/about-2.3e593410.jpg"
+  },
+  {
+    "revision": "3e593410f75fa5d081f3a18e6f563359",
+    "url": "https://arkanaputra.com/static/media/about.3e593410.jpg"
+  },
+  {
+    "revision": "17a1b230bf29246af179e1df091f41ab",
+    "url": "https://arkanaputra.com/static/media/blog-r-g.17a1b230.jpg"
+  },
+  {
+    "revision": "d006437becb84e03525bf006190b2a32",
+    "url": "https://arkanaputra.com/static/media/favicon.d006437b.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "https://arkanaputra.com/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "https://arkanaputra.com/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "https://arkanaputra.com/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "https://arkanaputra.com/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "https://arkanaputra.com/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0246ddceb6a4ffcc1bae9b05483ec904",
+    "url": "https://arkanaputra.com/static/media/fun-fact-bg.0246ddce.jpg"
+  },
+  {
+    "revision": "4ba75338fa59e981bf25bde6d79f5908",
+    "url": "https://arkanaputra.com/static/media/h-2-01.4ba75338.jpg"
+  },
+  {
+    "revision": "b1549d14e8d6de12a8639025b0a3a048",
+    "url": "https://arkanaputra.com/static/media/h-2-01.b1549d14.png"
+  },
+  {
+    "revision": "0125d3170222c40f302a37e11317b4e0",
+    "url": "https://arkanaputra.com/static/media/h-2-02.0125d317.jpg"
+  },
+  {
+    "revision": "e5a73e01b69e8374502fa9e6029a88b7",
+    "url": "https://arkanaputra.com/static/media/h-2-02.e5a73e01.png"
+  },
+  {
+    "revision": "869544a37a7f4822ed4e1647c3ba3ad7",
+    "url": "https://arkanaputra.com/static/media/h-2-03.869544a3.png"
+  },
+  {
+    "revision": "242dad24153ada30487931307507464e",
+    "url": "https://arkanaputra.com/static/media/h-2-04.242dad24.png"
+  },
+  {
+    "revision": "c2e0d865294af59f60e4bc68f2035b4c",
+    "url": "https://arkanaputra.com/static/media/h-2-t-01.c2e0d865.png"
+  },
+  {
+    "revision": "11a7e7d6cd7ee190ffeaf1369fd4023e",
+    "url": "https://arkanaputra.com/static/media/h-2-t-02.11a7e7d6.png"
+  },
+  {
+    "revision": "02e4a342818d8dd661f8434ef89071c7",
+    "url": "https://arkanaputra.com/static/media/h-2-t-03.02e4a342.png"
+  },
+  {
+    "revision": "ef733c387370fea49538f0ad0681ac14",
+    "url": "https://arkanaputra.com/static/media/page-header.ef733c38.jpg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "https://arkanaputra.com/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "aca3a2eef13767c702366d6021f3e1ab",
+    "url": "https://arkanaputra.com/static/media/service-bg.aca3a2ee.jpg"
+  },
+  {
+    "revision": "aca54e4d62bf00e7ef808e315e33ff03",
+    "url": "https://arkanaputra.com/static/media/slider-bg.aca54e4d.jpg"
+  },
+  {
+    "revision": "ec7a058d4dc4a58078984f585ae4d33f",
+    "url": "https://arkanaputra.com/static/media/team-bg-2.ec7a058d.jpg"
+  },
+  {
+    "revision": "3f284518090f05a859af1e6418a56978",
+    "url": "https://arkanaputra.com/static/media/team-details.3f284518.jpg"
+  },
+  {
+    "revision": "309edc077941230611f1dd502ef2d38b",
+    "url": "https://arkanaputra.com/static/media/team.309edc07.jpg"
+  }
+]);
